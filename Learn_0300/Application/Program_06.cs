3836e14f946260512680f44c6c4c34f684bce449
Dariush Tasdighi <dariusht@gmail.com>
2024-06-23 02:11:27 +0330

    Version 1.2.7 - Clean Code

--- Learn_0300/Application/Program_06.cs
+++ Learn_0300/Application/Program_06.cs
@@ -1,213 +1,213 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿//using System;
+//using Microsoft.EntityFrameworkCore;
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-try
-{
-	var username1 = $"Username1";
-	var username2 = $"Username2";
-	var username3 = $"Username3";
-	var username4 = $"Username4";
+//try
+//{
+//	var username1 = $"Username1";
+//	var username2 = $"Username2";
+//	var username3 = $"Username3";
+//	var username4 = $"Username4";
 
-	{
-		using var applicationDbContext = new ApplicationDbContext();
+//	{
+//		using var applicationDbContext = new ApplicationDbContext();
 
-		var hasAnyUser =
-			await
-			applicationDbContext.Users.AnyAsync();
+//		var hasAnyUser =
+//			await
+//			applicationDbContext.Users.AnyAsync();
 
-		if (hasAnyUser)
-		{
-			Console.WriteLine
-				(value: $"Please Delete Database First!");
+//		if (hasAnyUser)
+//		{
+//			Console.WriteLine
+//				(value: $"Please Delete Database First!");
 
-			return;
-		}
+//			return;
+//		}
 
-		User newUser;
-		UserProfile newUserProfile;
+//		User newUser;
+//		UserProfile newUserProfile;
 
-		// **************************************************
-		newUser =
-			new User(username: username1);
+//		// **************************************************
+//		newUser =
+//			new User(username: username1);
 
-		applicationDbContext.Add(entity: newUser);
-		// **************************************************
+//		applicationDbContext.Add(entity: newUser);
+//		// **************************************************
 
-		// **************************************************
-		newUser =
-			new User(username: username2);
+//		// **************************************************
+//		newUser =
+//			new User(username: username2);
 
-		newUserProfile =
-			new UserProfile(fullName: "Full Name (2)")
-			{
-				UserId = newUser.Id,
-			};
+//		newUserProfile =
+//			new UserProfile(fullName: "Full Name (2)")
+//			{
+//				UserId = newUser.Id,
+//			};
 
-		applicationDbContext.Add(entity: newUser);
-		applicationDbContext.Add(entity: newUserProfile);
-		// **************************************************
+//		applicationDbContext.Add(entity: newUser);
+//		applicationDbContext.Add(entity: newUserProfile);
+//		// **************************************************
 
-		// **************************************************
-		newUser =
-			new User(username: username3);
+//		// **************************************************
+//		newUser =
+//			new User(username: username3);
 
-		newUserProfile =
-			new UserProfile(fullName: "Full Name (3)")
-			{
-				User = newUser,
-			};
+//		newUserProfile =
+//			new UserProfile(fullName: "Full Name (3)")
+//			{
+//				User = newUser,
+//			};
 
-		applicationDbContext.Add(entity: newUser);
-		applicationDbContext.Add(entity: newUserProfile);
-		// **************************************************
+//		applicationDbContext.Add(entity: newUser);
+//		applicationDbContext.Add(entity: newUserProfile);
+//		// **************************************************
 
-		// **************************************************
-		newUser =
-			new User(username: username4);
+//		// **************************************************
+//		newUser =
+//			new User(username: username4);
 
-		newUserProfile =
-			new UserProfile(fullName: "Full Name (4)");
+//		newUserProfile =
+//			new UserProfile(fullName: "Full Name (4)");
 
-		newUser.Profile = newUserProfile;
+//		newUser.Profile = newUserProfile;
 
-		applicationDbContext.Add(entity: newUser);
-		// **************************************************
+//		applicationDbContext.Add(entity: newUser);
+//		// **************************************************
 
-		var affectedRows =
-			await
-			applicationDbContext.SaveChangesAsync();
+//		var affectedRows =
+//			await
+//			applicationDbContext.SaveChangesAsync();
 
-		Console.WriteLine
-			(value: $"{nameof(affectedRows)}: {affectedRows}");
-	}
-}
-catch (Exception ex)
-{
-	Console.WriteLine(value: ex.Message);
-}
+//		Console.WriteLine
+//			(value: $"{nameof(affectedRows)}: {affectedRows}");
+//	}
+//}
+//catch (Exception ex)
+//{
+//	Console.WriteLine(value: ex.Message);
+//}
 
-public abstract class Entity : object
-{
-	[Key]
-	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
-	public Guid Id { get; private set; } = Guid.NewGuid();
+//public abstract class Entity : object
+//{
+//	[Key]
+//	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+//	public Guid Id { get; private set; } = Guid.NewGuid();
 
-	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
-	public DateTimeOffset InsertDateTime { get; private set; } = DateTimeOffset.Now;
-}
+//	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+//	public DateTimeOffset InsertDateTime { get; private set; } = DateTimeOffset.Now;
+//}
 
-public class User(string username) : Entity
-{
-	/// <summary>
-	/// نکته مهم - دستور ذیل را نباید بنویسیم
-	/// </summary>
-	//public Guid? ProfileId { get; set; }
+//public class User(string username) : Entity
+//{
+//	/// <summary>
+//	/// نکته مهم - دستور ذیل را نباید بنویسیم
+//	/// </summary>
+//	//public Guid? ProfileId { get; set; }
 
-	public virtual UserProfile? Profile { get; set; }
+//	public virtual UserProfile? Profile { get; set; }
 
-	[MaxLength(length: 20)]
-	[Required(AllowEmptyStrings = false)]
-	public string Username { get; set; } = username;
-}
+//	[MaxLength(length: 20)]
+//	[Required(AllowEmptyStrings = false)]
+//	public string Username { get; set; } = username;
+//}
 
-public class UserProfile(string fullName) : Entity
-{
-	[Required]
-	public Guid UserId { get; set; }
+//public class UserProfile(string fullName) : Entity
+//{
+//	[Required]
+//	public Guid UserId { get; set; }
 
-	public virtual User? User { get; set; }
-	//public virtual User User { get; set; } = null!;
+//	public virtual User? User { get; set; }
+//	//public virtual User User { get; set; } = null!;
 
-	[MaxLength(length: 50)]
-	[Required(AllowEmptyStrings = false)]
-	public string FullName { get; set; } = fullName;
-}
+//	[MaxLength(length: 50)]
+//	[Required(AllowEmptyStrings = false)]
+//	public string FullName { get; set; } = fullName;
+//}
 
-internal class UserConfiguration :
-	object, IEntityTypeConfiguration<User>
-{
-	public UserConfiguration() : base()
-	{
-	}
+//internal class UserConfiguration :
+//	object, IEntityTypeConfiguration<User>
+//{
+//	public UserConfiguration() : base()
+//	{
+//	}
 
-	public void Configure(EntityTypeBuilder<User> builder)
-	{
-		builder
-			.HasKey(current => current.Id)
-			.IsClustered(clustered: false)
-			;
+//	public void Configure(EntityTypeBuilder<User> builder)
+//	{
+//		builder
+//			.HasKey(current => current.Id)
+//			.IsClustered(clustered: false)
+//			;
 
-		builder
-			.Property(current => current.Username)
-			.IsUnicode(unicode: false)
-			;
+//		builder
+//			.Property(current => current.Username)
+//			.IsUnicode(unicode: false)
+//			;
 
-		builder
-			.HasIndex(current => new { current.Username })
-			.IsUnique(unique: true)
-		;
+//		builder
+//			.HasIndex(current => new { current.Username })
+//			.IsUnique(unique: true)
+//			;
 
-		builder
-			.HasOne(current => current.Profile)
-			.WithOne(other => other.User)
-			.HasForeignKey<UserProfile>(other => other.UserId)
-			.IsRequired(required: true)
-			;
-	}
-}
+//		builder
+//			.HasOne(current => current.Profile)
+//			.WithOne(other => other.User)
+//			.HasForeignKey<UserProfile>(other => other.UserId)
+//			.IsRequired(required: true)
+//			;
+//	}
+//}
 
-internal class UserProfileConfiguration :
-	object, IEntityTypeConfiguration<UserProfile>
-{
-	public UserProfileConfiguration() : base()
-	{
-	}
+//internal class UserProfileConfiguration :
+//	object, IEntityTypeConfiguration<UserProfile>
+//{
+//	public UserProfileConfiguration() : base()
+//	{
+//	}
 
-	public void Configure(EntityTypeBuilder<UserProfile> builder)
-	{
-		builder
-			.HasKey(current => current.Id)
-			.IsClustered(clustered: false)
-			;
+//	public void Configure(EntityTypeBuilder<UserProfile> builder)
+//	{
+//		builder
+//			.HasKey(current => current.Id)
+//			.IsClustered(clustered: false)
+//			;
 
-		builder
-			.HasIndex(current => new { current.FullName })
-			.IsUnique(unique: false)
-			;
-	}
-}
+//		builder
+//			.HasIndex(current => new { current.FullName })
+//			.IsUnique(unique: false)
+//			;
+//	}
+//}
 
-public class ApplicationDbContext : DbContext
-{
-	public ApplicationDbContext() : base()
-	{
-		Database.EnsureCreated();
-	}
+//public class ApplicationDbContext : DbContext
+//{
+//	public ApplicationDbContext() : base()
+//	{
+//		Database.EnsureCreated();
+//	}
 
-	public DbSet<User> Users { get; set; }
-	public DbSet<UserProfile> UserProfiles { get; set; }
+//	public DbSet<User> Users { get; set; }
+//	public DbSet<UserProfile> UserProfiles { get; set; }
 
-	protected override void OnConfiguring
-		(DbContextOptionsBuilder optionsBuilder)
-	{
-		var connectionString =
-			"Server=.;User ID=sa;Password=1234512345;Database=LEARNING_EF_CORE_0300;MultipleActiveResultSets=true;TrustServerCertificate=True;";
+//	protected override void OnConfiguring
+//		(DbContextOptionsBuilder optionsBuilder)
+//	{
+//		var connectionString =
+//			"Server=.;User ID=sa;Password=1234512345;Database=LEARNING_EF_CORE_0300;MultipleActiveResultSets=true;TrustServerCertificate=True;";
 
-		//optionsBuilder
-		//	.LogTo(action: Console.WriteLine)
-		//	;
+//		//optionsBuilder
+//		//	.LogTo(action: Console.WriteLine)
+//		//	;
 
-		optionsBuilder
-			.UseSqlServer(connectionString: connectionString)
-			;
-	}
+//		optionsBuilder
+//			.UseSqlServer(connectionString: connectionString)
+//			;
+//	}
 
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		modelBuilder.ApplyConfigurationsFromAssembly
-			(assembly: typeof(ApplicationDbContext).Assembly);
-	}
-}
+//	protected override void OnModelCreating(ModelBuilder modelBuilder)
+//	{
+//		modelBuilder.ApplyConfigurationsFromAssembly
+//			(assembly: typeof(ApplicationDbContext).Assembly);
+//	}
+//}
---
+++ Learn_0300/Application/Program_07.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+try
+{
+	var username = $"Dariush Tasdighi";
+
+	{
+		using var applicationDbContext = new ApplicationDbContext();
+
+		var hasAnyUser =
+			await
+			applicationDbContext.Users.AnyAsync();
+
+		if (hasAnyUser)
+		{
+			Console.WriteLine
+				(value: $"Please Delete Database First!");
+
+			return;
+		}
+
+		var newUser =
+			new User(username: username);
+
+		applicationDbContext.Add(entity: newUser);
+
+		var affectedRows =
+			await
+			applicationDbContext.SaveChangesAsync();
+
+		Console.WriteLine
+			(value: $"{nameof(affectedRows)}: {affectedRows}");
+	}
+}
+catch (Exception ex)
+{
+	Console.WriteLine(value: ex.Message);
+}
+
+public abstract class Entity : object
+{
+	[Key]
+	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+	public Guid Id { get; private set; } = Guid.NewGuid();
+
+	[DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.None)]
+	public DateTimeOffset InsertDateTime { get; private set; } = DateTimeOffset.Now;
+}
+
+public class User(string username) : Entity
+{
+	[MaxLength(length: 20)]
+	[Required(AllowEmptyStrings = false)]
+	public string Username { get; set; } = username;
+
+	public string? String1 { get; set; }
+	public string? String2 { get; set; }
+	public string? String3 { get; set; }
+	public string? String4 { get; set; }
+	public string? String5 { get; set; }
+	public string? String6 { get; set; }
+}
+
+internal class UserConfiguration :
+	object, IEntityTypeConfiguration<User>
+{
+	public UserConfiguration() : base()
+	{
+	}
+
+	public void Configure(EntityTypeBuilder<User> builder)
+	{
+		builder
+			.HasKey(current => current.Id)
+			.IsClustered(clustered: false)
+			;
+
+		builder
+			.Property(current => current.Username)
+			.IsUnicode(unicode: false)
+			;
+
+		builder
+			.HasIndex(current => new { current.Username })
+			.IsUnique(unique: true)
+			;
+
+		// Char(10)
+		builder
+			.Property(current => current.String1)
+			.IsUnicode(unicode: false)
+			.HasMaxLength(maxLength: 10)
+			.IsFixedLength(fixedLength: true)
+			;
+
+		// NChar(10)
+		builder
+			.Property(current => current.String2)
+			.IsUnicode(unicode: true)
+			.HasMaxLength(maxLength: 10)
+			.IsFixedLength(fixedLength: true)
+			;
+
+		// Varchar(10)
+		builder
+			.Property(current => current.String3)
+			.IsUnicode(unicode: false)
+			.HasMaxLength(maxLength: 10)
+			.IsFixedLength(fixedLength: false)
+			;
+
+		// NVarchar(10) - OK
+		builder
+			.Property(current => current.String4)
+			.IsUnicode(unicode: true)
+			.HasMaxLength(maxLength: 10)
+			.IsFixedLength(fixedLength: false)
+			;
+
+		// Varchar(Max)
+		builder
+			.Property(current => current.String5)
+			.IsUnicode(unicode: false)
+			//.HasMaxLength(maxLength: 10)
+			.IsFixedLength(fixedLength: false)
+			;
+
+		// NVarchar(Max) - OK
+		builder
+			.Property(current => current.String6)
+			.IsUnicode(unicode: true)
+			//.HasMaxLength(maxLength: 10)
+			.IsFixedLength(fixedLength: false)
+			;
+	}
+}
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext() : base()
+	{
+		Database.EnsureCreated();
+	}
+
+	public DbSet<User> Users { get; set; }
+
+	protected override void OnConfiguring
+		(DbContextOptionsBuilder optionsBuilder)
+	{
+		var connectionString =
+			"Server=.;User ID=sa;Password=1234512345;Database=LEARNING_EF_CORE_0300;MultipleActiveResultSets=true;TrustServerCertificate=True;";
+
+		//optionsBuilder
+		//	.LogTo(action: Console.WriteLine)
+		//	;
+
+		optionsBuilder
+			.UseSqlServer(connectionString: connectionString)
+			;
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly
+			(assembly: typeof(ApplicationDbContext).Assembly);
+	}
+}
