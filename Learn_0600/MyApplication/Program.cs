03658926b17015ba9066193a448e099d5ba3ae83
Dariush Tasdighi <dariusht@gmail.com>
2024-10-28 19:20:09 +0330

    Version 1.4.6 - Adding Learn 600 - Migration - Update Learning Migration Word File

---
+++ Learn_0600/MyApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Application.Persistence;
+
+try
+{
+	using var applicationDbContext = new ApplicationDbContext();
+
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
---
+++ Learn_0600/Persistence/Configurations/CountryConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Persistence.Configurations;
+
+internal sealed class CountryConfiguration : object, IEntityTypeConfiguration<Country>
+{
+	public void Configure(EntityTypeBuilder<Country> builder)
+	{
+		// **************************************************
+		builder
+			.HasIndex(current => new { current.Name })
+			.IsUnique(unique: true)
+			;
+		// **************************************************
+
+		// **************************************************
+		for (var index = 1; index <= 5; index++)
+		{
+			var data =
+				new Country(name: $"Country {index}")
+				{
+					Id = index,
+					Code = index,
+				};
+
+			builder.HasData(data: data);
+		}
+		// **************************************************
+	}
+}
